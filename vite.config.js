f538a01fbbda7710caf8229840f74b516a8d4924
yumasou <heinhtetpaing2273@gmail.com>
2025-01-21 14:27:15 +0700

    jan222025

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host:"0.0.0.0",
     port:process.env.PORT || 3000,
     open: true,
     hmr: true,
